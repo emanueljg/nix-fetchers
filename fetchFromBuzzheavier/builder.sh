adc93e83367fad17dce33b013212ce82c14a293c
Emanuel Johnson Godin <emanueljohnsongodin@gmail.com>
2025-10-20 20:40:05 +0200

    Update builder.sh

--- fetchFromBuzzheavier/builder.sh
+++ fetchFromBuzzheavier/builder.sh
@@ -23,36 +23,7 @@ if [[ "$useSHA1Hash" == "true" ]]; then
   set +x
 else
   echo "using nix's hash..."
-  
 
-  curl 'https://buzzheavier.com/frtjt00auxv5' | pup 'div[x-data="App()"] span:first-child, div[x-data="App()"] li:nth-child(4) code text{}'                   
-else
-fi
-
-auth_token=$(cat "$apiKeyPath")
-
-set -o nogl
-api_curl=("${curl[@]}" -H "Authorization: Bearer $auth_token")
-
-echo 'Fetching game ID...'
-game_id=$("${curl[@]}" "https://$creator.itch.io/$game/data.json" | jq -r '.id')
-
-echo 'Fetching upload ID...'
-upload_id=$(
-  "${api_curl[@]}" \
-    "https://itch.io/api/1/key/game/$game_id/uploads" \
-  | jq -r --arg upload_name "$uploadName" \
-    '.uploads[] | select(.filename == $upload_name) | .id'
-)
-
-echo 'Fetching download URL...'
-download_url=$(
-  "${api_curl[@]}" \
-    "https://itch.io/api/1/key/upload/$upload_id/download" \
-  | jq -r '.url'
-)
-
-echo 'Downloading game...'
-"${curl[@]}" "$download_url" -o "$out"
+echo "${!hash@A}"
 
 set +o noglob
