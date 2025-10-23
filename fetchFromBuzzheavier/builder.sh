fc9bb8a2fe165195c69114d1c232dbfa61501d1d
Emanuel Johnson Godin <emanueljohnsongodin@gmail.com>
2025-10-24 00:38:25 +0200

    Update builder.sh

--- fetchFromBuzzheavier/builder.sh
+++ fetchFromBuzzheavier/builder.sh
@@ -4,41 +4,26 @@
 if [ -e .attrs.sh ]; then source .attrs.sh; fi
 source "${stdenv:?}/setup"
 
-curlVersion=$(curl -V | head -1 | cut -d' ' -f2)
+echo "Asking Buzzheavier for download link to $item ($url)..."
+download_url="$(
+    curl "$url" \
+    --head
+    --silent \
+    --output /dev/null \
+    --header 'Referer: https://buzzheavier.com/frtjt00auv5' \
+    --write-out '%header{hx-redirect}'
+)"
 
-curl=(
-    curl
-    --location
-    --max-redirs 20
-    --retry 3
-    --retry-all-errors
-    --continue-at -
-    --disable-epsv
-    --user-agent "curl/$curlVersion Nixpkgs/$nixpkgsVersion"
-)
+echo "Buzzheavier successfully responded with download link '$download_url'. Downloading..."
+curl "$u
 
-echo "Downloading BH item $item ($url)"
-echo "Fetching HTML and parsing metadata..."
-"${curl[@]}" -4 -v 'https://buzzheavier.com/frtjt00auxv5'
-read -r -d "\n" buzzheavier_name buzzheavier_sha1 <$("${curl[@]}" -v "$url" \
-  | pup 'div[x-data="App()"] span:first-child, div[x-data="App()"] li:nth-child(4) code text{}'
-)
 
-set -x
-echo $buzzheavier_name
-echo $buzzheavier_sha1
-set +x
-
-echo -n "useSHA1Hash is "$useSHA1Hash: " 
-if [[ "$useSHA1Hash" == "true" ]]; then
-  echo "using buzzheavier's hash..."
-  set -x
-  hash="$buzzheavier_sha1"
-  hashAlgo="sha1"
-  set +x
-else
-  echo "using nix's hash..."
-
-set -x
-echo "$hash"
-set +x
+if [[ "$requestInfo" == "true" ]]; then
+    read -r -d "\n" buzzheavier_name buzzheavier_sha1 <<<$(curl -s "$url" \
+      | pup '.text-2xl, code text{}'
+    )
+    set -x
+    echo $buzzheavier_name
+    echo $buzzheavier_sha1
+    set +x
+fi
