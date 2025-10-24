b00d84ba0f4c12be73ed13e6a66b7037bc95f074
Emanuel Johnson Godin <emanueljohnsongodin@gmail.com>
2025-10-24 15:11:20 +0200

    Update lib.nix

--- lib.nix
+++ lib.nix
@@ -1,32 +1,24 @@
 { pkgs }: let
   inherit (pkgs) lib;
 {
-  # this helper is ripped out of `pkgs.writeShellApplication` 
-  # and adds dry-run and shellcheck checks
+  # this helper is ripped out of `pkgs.writeShellApplication` but simplified a bit
+  # it adds dry-run and shellcheck checks
+  #
   makeBashCheckPhase = {
-    excludes ? [ ]
+    opts ? { }
   }: let
-    excludeFlags = lib.optionals (excludeShellChecks != [ ]) [
-      "--exclude"
-      (lib.concatStringsSep "," excludes)
-    ];
     # GHC (=> shellcheck) isn't supported on some platforms (such as risc-v)
     # but we still want to use writeShellApplication on those platforms
     shellcheckCommand = lib.optionalString pkgs.shellcheck-minimal.compiler.bootstrapAvailable ''
       # use shellcheck which does not include docs
       # pandoc takes long to build and documentation isn't needed for just running the cli
-      ${lib.getExe pkgs.shellcheck-minimal} ${
-        lib.escapeShellArgs (excludeFlags ++ extraShellCheckFlags)
-      } "$target"
+      ${lib.getExe pkgs.shellcheck-minimal} "$target" \
+        ${cli.toGNUCommandLineShell { } opts}
     '';
-  in
-  if checkPhase == null then
-    ''
+  in ''
       runHook preCheck
       ${stdenv.shellDryRun} "$target"
       ${shellcheckCommand}
       runHook postCheck
-    ''
-  else
-    checkPhase;
+    '';
 }
