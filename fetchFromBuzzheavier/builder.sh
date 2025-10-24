b4377721cfc3d324115306c8b1fe4a86489a5f40
Emanuel Johnson Godin <emanueljohnsongodin@gmail.com>
2025-10-24 13:35:16 +0200

    Add error handling for missing download URL

--- fetchFromBuzzheavier/builder.sh
+++ fetchFromBuzzheavier/builder.sh
@@ -13,5 +13,10 @@ download_url="$(
     --write-out '%header{hx-redirect}'
 )"
 
+if [ -z "$download_url" ]; then
+    echo "ERROR: Buzzheavier gave us no $download_url on our request."
+    exit 1
+fi
+
 echo "Buzzheavier successfully responded with download link $download_url. Downloading..."
 curl "$download_url" --output $out
