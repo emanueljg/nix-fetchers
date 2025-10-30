26f7ca28fe2dbd1404954622bc8077f7d9cfb619
Emanuel Johnson Godin <emanueljohnsongodin@gmail.com>
2025-10-30 09:42:23 +0100

    Update sources.nix

--- sources.nix
+++ sources.nix
@@ -1,9 +1,10 @@
 {
+  # track: nixos-unstable
   nixpkgs = builtins.fetchTree {
     type = "github";
     owner = "nixos";
     repo = "nixpkgs";
-    ref = "nixos-unstable";
+    rev = "08dacfca559e1d7da38f3cf05f1f45ee9bfd213c";
     narHash = "";
   };
 }
