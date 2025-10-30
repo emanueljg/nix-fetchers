28f983ff4c64193a0a93629e76042d8c936001a3
Emanuel Johnson Godin <emanueljohnsongodin@gmail.com>
2025-10-30 09:43:07 +0100

    Update sources.nix

--- sources.nix
+++ sources.nix
@@ -5,6 +5,6 @@
     owner = "nixos";
     repo = "nixpkgs";
     rev = "08dacfca559e1d7da38f3cf05f1f45ee9bfd213c";
-    narHash = "";
+    narHash = "sha256-o9KF3DJL7g7iYMZq9SWgfS1BFlNbsm6xplRjVlOCkXI=";
   };
 }
